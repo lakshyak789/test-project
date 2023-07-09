e92da6dcde75ff21f0b4ac3008e33e47cb537b50
Lakshya Khanna <lakshyak789@gmail.com>
2023-07-09 20:20:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ChakraProvider, Box, Grid, Image, Text, Heading, Center } from '@chakra-ui/react';
+import { useQuery } from "graphql-hooks";
+import './App.css'
+
+const HOMEPAGE_QUERY = `query {
+  sadhguruFigmaApp {
+    columnStructure {
+      benefits
+      image {
+        url
+      }
+    }
+    bgImage {
+      url
+    }
+    headingImg {
+      url
+    }
+    heading
+  }
+}`;
 
 function App() {
+  const { loading, error, data } = useQuery(HOMEPAGE_QUERY, {
+    variables: {
+      limit: 10
+    }
+  });
+  if (loading) return "Loading...";
+  if (error) return "Something Bad Happened";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    // <ChakraProvider>
+    //   <Box p={4}
+    //   backgroundImage={`url(${data.sadhguruFigmaApp.bgImage.url})`}
+    //   backgroundSize="cover"
+    //   minHeight="713px"
+    //   display="flex"
+    //   alignItems="center"
+    //   justifyContent="center"
+    //   clipPath= "polygon(0% 0%, 100% 10%, 101% 95%, 0% 95%)"
+    //   >
+    //     <Box textAlign="center" width="100%">
+    //   <Heading as="h1" size="xl" mt={8}>
+    //     Heading
+    //   </Heading>
+    //   <Center mt={4}>
+    //     <Image src="path/to/image.png" alt="Image" />
+    //   </Center>
+      
+    // </Box>
+    //   <Grid templateColumns="repeat(5, 1fr)" gap={12} 
+    //     maxWidth="80%"
+    //     margin="auto">
+    //       {data.sadhguruFigmaApp.columnStructure.map((column, index) => (
+    //         <Box key={index} height="0px">
+    //           <Image 
+    //           margin="auto" src={column.image.url} alt={`Image ${index}`} />
+    //           <Text
+    //            textAlign="center"
+    //            color="white" mt={2}>{column.benefits}</Text>
+    //         </Box>
+    //       ))}
+    //     </Grid>
+    //   </Box>
+    // </ChakraProvider>
+
+   
+    <Box
+      backgroundImage={`url(${data.sadhguruFigmaApp.bgImage.url})`}
+      backgroundSize="cover"
+      backgroundPosition="center"
+      height= "100vh"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      clipPath= "polygon(0% 0%, 100% 10%, 101% 95%, 0% 95%)"
+    >
+      <Center flexDirection="column">
+        <Heading as="h1" size="xl" color="white" textAlign="center" mb={8}>
+        {`${data.sadhguruFigmaApp.heading}`}
+        </Heading>
+        <Image
+          src={`${data.sadhguruFigmaApp.headingImg.url}`}
+          alt="Logo"
+          marginBottom="5rem"
+        />
+        <Grid
+          templateColumns="repeat(5, 1fr)"
+          gap={16}
+          justifyContent="center"
+          alignItems="center"
+          maxWidth="80%"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {data.sadhguruFigmaApp.columnStructure.map((column, index) => (
+            <Box key={index}>
+              <Image 
+              margin="auto" src={column.image.url} alt={`Image ${index}`} />
+              <Text
+               textAlign="center"
+               color="white" mt={2}>{column.benefits}</Text>
+            </Box>
+          ))}
+        </Grid>
+      </Center>
+    </Box>
   );
-}
+};
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+import { GraphQLClient, ClientContext } from 'graphql-hooks'
+import { ChakraProvider } from '@chakra-ui/react'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const client = new GraphQLClient({
+  url: "https://graphql.datocms.com/",
+  headers: {
+    "Authorization": "Bearer 61d69d77079ead57fd405e445e9e3a",
+  }
+});
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ClientContext.Provider value={client}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ClientContext.Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
